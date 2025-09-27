4d083d365e422e5e7c922c149beeb23f4cfc6c00
Mudit Bengani <muditbengani06@gmail.com>
2025-09-27 13:22:05 +0530

    commented out problematic code

--- src/main.c
+++ src/main.c
@@ -8,11 +8,11 @@
 void main() {
     uart_init();
     init_printf(0, putc);
-    irq_vector_init();
-    enable_interrupt_controller();
-    enable_irq();
-    int el_code = get_el();
-    printf("The elcode is %d \n", el_code);
+    //irq_vector_init();
+    //enable_interrupt_controller();
+    //enable_irq();
+    //int el_code = get_el();
+    //printf("The elcode is %d \n", el_code);
     memory_init();
     uart_putc('\r\n');
     shell();
